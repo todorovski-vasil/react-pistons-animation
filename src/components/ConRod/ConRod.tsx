12737fcc1301d0abe320c2ea88968a74f7e0e05a
Vasil Todorovski <vasil.todorovski@symphony-solutions.eu>
2020-05-29 12:59:14 +0200

    Fixed animation timing
    - synced ConRod compound animation
    - synced Piston animation speed to ConRod connecting bolt

--- src/components/ConRod/ConRod.tsx
+++ src/components/ConRod/ConRod.tsx
@@ -2,57 +2,7 @@ import React from 'react';
 
 import classes from './ConRod.module.css';
 
-interface StateInterface {
-    phase: number;
-    phaseCss: string;
-}
-
-interface ActionInterface {
-    type: string;
-    payload?: any;
-}
-
-// const phases: Array<string> = [
-//     classes.tdc,
-//     classes.ldc,
-//     classes.bdc,
-//     classes.rdc,
-// ];
-
-// const initialState: StateInterface = {
-//     phase: 3,
-//     phaseCss: phases[3],
-// };
-
-// const reducer: React.Reducer<StateInterface, ActionInterface> = (
-//     prevState: StateInterface,
-//     action: ActionInterface
-// ) => {
-//     switch (action.type) {
-//         case 'NEXT_STAGE':
-//             return {
-//                 ...prevState,
-//                 phase: (prevState.phase + 1) % 4,
-//                 phaseCss: phases[(prevState.phase + 1) % 4],
-//             };
-//         default:
-//             return prevState;
-//     }
-// };
-
 function ConRod(props: object) {
-    // const [state, dispatch] = useReducer(reducer, initialState);
-
-    // useEffect(() => {
-    //     const cycle = setInterval(() => {
-    //         dispatch({ type: 'NEXT_STAGE' });
-    //     }, 500);
-
-    //     return () => {
-    //         clearInterval(cycle);
-    //     };
-    // }, []);
-
     return (
         <div className={classes.conrodContainer}>
             <div className={classes.conrod}>
--- src/components/piston/Piston.tsx
+++ src/components/piston/Piston.tsx
@@ -2,28 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 import classes from './Piston.module.css';
 
-// enum Position {
-//     TDC = 0,
-//     BDC = 1,
-// }
-
 function Piston(props: object) {
-    const [cssPhase, setCssPhase] = useState(classes.bdc);
-
-    useEffect(() => {
-        const cycle = setInterval(() => {
-            setCssPhase((cssPhase) =>
-                cssPhase === classes.bdc ? classes.tdc : classes.bdc
-            );
-        }, 1000);
-
-        return () => {
-            clearInterval(cycle);
-        };
-    }, []);
-
     return (
-        <div className={classes.piston + ' ' + cssPhase}>
+        <div className={classes.piston}>
             <div className={classes.ring1}></div>
             <div className={classes.ring2}></div>
             <div className={classes.conBolt}></div>
